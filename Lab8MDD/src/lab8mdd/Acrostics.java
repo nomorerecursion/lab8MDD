801f70caf7d8f240a75ac6e7eebf366359b481ec
s508609 <s508609@CH115012.nwmissouri.edu>
2013-03-14 16:45:00 -0500

    Added methods to Acrostics

--- Lab8MDD/src/lab8mdd/Acrostics.java
+++ Lab8MDD/src/lab8mdd/Acrostics.java
@@ -1,9 +1,55 @@
 package lab8mdd;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * @author Matt Seipel
  */
 public class Acrostics 
 {
-
+	
+	private String wordIn;
+	/**
+	 * Create the Acrostics game board (in progess)
+	 * @param wordIn 
+	 */
+	public Acrostics(String wordIn){
+		this.wordIn = wordIn.toUpperCase();
+		createGameBoard();
+	}
+	
+	/**
+	 * Default 
+	 */
+	public Acrostics(){
+		super();
+	}
+	
+	public void createDictionary(){
+		try{
+			Scanner readDictionary = 
+				new Scanner(new File("corncob_lowercase"));
+			ArrayList<String> dictionaryList= new ArrayList<>();
+			while(readDictionary.hasNext()){
+				dictionaryList.add(readDictionary.nextLine());
+			}
+			System.out.println(dictionaryList);
+		}
+		catch(FileNotFoundException e){
+			System.out.println("404: File Not Found");
+		}
+		
+		
+		
+		
+	}
+	private void createGameBoard(){
+		for(int i=1; i<wordIn.length()+1; i++){
+			System.out.println(wordIn.charAt(i-1) + "-----" 
+				+ wordIn.charAt(wordIn.length()-i));
+		}
+	}
 }
--- Lab8MDD/src/lab8mdd/Lab8MDD.java
+++ Lab8MDD/src/lab8mdd/Lab8MDD.java
@@ -8,5 +8,6 @@ public class Lab8MDD
    public static void main(String[] args) 
 	{
 		
+		
    }
 }
