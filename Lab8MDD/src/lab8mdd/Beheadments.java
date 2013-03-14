0d66cc0535dadeabfe6fb7040f297ef7728a5c1f
S401544 <S401544@CH115010.nwmissouri.edu>
2013-03-14 17:24:38 -0500

    Final update for 3/14/13

--- Lab8MDD/src/lab8mdd/Beheadments.java
+++ Lab8MDD/src/lab8mdd/Beheadments.java
@@ -12,10 +12,32 @@ public class Beheadments
 {
 
 	ArrayList<String> dictionary = new ArrayList<String>();
+	ArrayList<String> player1Array = new ArrayList<String>();
+	
+	
+	public void player1(){
+		Scanner input = new Scanner(System.in);
+		String player1;
+		System.out.println("Enter as many words as you can seperated with a comma starting now: ");
+		player1 = input.nextLine();
+		
+		String[] temp = player1.split(", ");
+		for(int i = 0; i < temp.length; i++){
+			player1Array.add(temp[i]);
+		}
+		
+		for(int i = 0; i < player1Array.size(); i++){
+			System.out.print(player1Array.get(i) + " ");
+		}
+	}
 	
 	
 	
 	
+	
+	/*
+	 * adds all the elements of the file to an arrayList
+	 */
 	public void processDictionary(String filename)throws FileNotFoundException {
 		
 		File myFile = new File(filename);
@@ -27,6 +49,10 @@ public class Beheadments
 			
 				dictionary.add(readLine);
 			
+		}
+		
+		for(int i = 0; i < dictionary.size(); i++){
+			System.out.println(dictionary.get(i));
 		}
 		
 		
