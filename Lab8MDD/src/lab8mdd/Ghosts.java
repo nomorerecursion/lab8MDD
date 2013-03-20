effd4eab2cd7a99b02811225c6365e39fecf2e32
Matt Seipel <Matt Seipel@MattS-Desktop>
2013-03-20 13:40:37 -0500

    Completed Ghosts (reworked checkWord method, playGhosts method, and Jarvis's word selection sequence).  Wrote the code for the menu on main.  Commented out "case 0" as that is where Daniel's game will go.  Feel free to change the menu around, I tried to make it as clean as possible.  Lastly, I "Alt+Shift+F"-ed the main file to clean up the code.

--- Lab8MDD/src/lab8mdd/Ghosts.java
+++ Lab8MDD/src/lab8mdd/Ghosts.java
@@ -9,73 +9,128 @@ public class Ghosts {
 
     private Scanner userInput = new Scanner(System.in);
     private String word;
-    private StringBuilder wordInProgress;
+    private StringBuilder wordInProgress = new StringBuilder();
     private String potentialWord;
+    private boolean endGame = false;
+    private int turn;
 
     public void playGhosts() {
+        //Initiate the dictionary.
         Jarvis.createDictionary();
-
+        //Print out instructions
+        System.out.println("Objective: Do NOT complete a word.  "
+                + "Enter one letter per turn.");
+        //Allow player 1 to make a move to initialize word and wordInProgress.
+        turn = 1;
         System.out.print("Player 1: ");
         word = userInput.next();
+        //Append the letter choice to wordInProgress
+        wordInProgress.append(word);
 
-        while (checkWord(word) == false) {
+        //Continue the game while a word is still possible and has not been completed.
+        while (endGame == false) {
+            //Player 2's turn
+            turn = 2;
             word = addLetterJarvis(word);
+            endGame = checkWord(word);
 
+            //Check if the word has been completed or nerfed between turns.
+            if (endGame == true) {
+                break;
+            }
+
+            //Player 1's turn
+            turn = 1;
             word = addLetter(word);
+            endGame = checkWord(word);
         }
 
+        //Determine which player has won the game.
+        if (turn == 1) {
+            System.out.println("Game Over! Victor: Player 2");
+        } else {
+            System.out.println("Game Over! Victor: Player 1");
+        }
 
     }
 
+    /**
+     * Checks the current word to see if it is a complete word, or if any
+     * possible words can be made using it as a prefix. If so, return false, in
+     * order to continue game conditions. If not, return true,
+     *
+     * @param gameWord
+     * @return true if a word is made or does not exist, false if a word is
+     * still possible.
+     */
     private boolean checkWord(String gameWord) {
         for (String s : Jarvis.dictionary) {
-            if (s.startsWith(gameWord)) {
-                return false;
-            } else if (Jarvis.dictionary.contains(gameWord)) {
-                return true;
+            if (s.equals(gameWord)) {
+                return true; //Completed word.
+            } else if (s.startsWith(gameWord) && s.length() > gameWord.length()) {
+                return false; //Further possibilities
             }
         }
-        /*
-         * No condition is met.
-         * The word is neither part of a word, nor an actual word.
-         * Ends game, player loses.
-         */
+        //Concludes that no further words are possible.
         return true;
     }
 
+    /**
+     * User's turn to make a move. Prompt for input and append to
+     * wordInProgress.
+     *
+     * @param gameWord
+     * @return updated game word.
+     */
     private String addLetter(String gameWord) {
-        wordInProgress = new StringBuilder(gameWord);
         System.out.print("Player 1: " + gameWord);
         wordInProgress = wordInProgress.append(userInput.next());
 
-        //Check if adding the letter creates a valid word and ends game.
-        if (checkWord(wordInProgress.toString()) == true) {
-            return "Game Over!  Victor: Player 2";
-        } else {
-            return wordInProgress.toString();
-        }
+        return wordInProgress.toString();
+
     }
 
+    /**
+     * Allow Jarvis to make a move. Checks for potential words and continues to
+     * append letters, while not completing a word, unless there is no further
+     * possibilities.
+     *
+     * @param gameWord
+     * @return updated game word.
+     */
     private String addLetterJarvis(String gameWord) {
-        wordInProgress = new StringBuilder(gameWord);
-        System.out.print("Player 2: ");
-        
+        //Boolean variable set up to determine if Jarvis can makes a legal move.
+        boolean move = false;
+
+        //Process the dictionary for most recent potential words.
         for (String s : Jarvis.dictionary) {
+            //Make sure potential words use the current game word as a prefix.
+            //Also, the potential word must be longer than game word.
             if (s.startsWith(gameWord) && s.length() > gameWord.length()) {
                 potentialWord = s;
+                //Make sure if the next letter is appended it won't complete a word.
                 if (!Jarvis.dictionary.contains(
                         potentialWord.substring(0, gameWord.length() + 1))) {
-                    wordInProgress = wordInProgress.append(s.charAt(gameWord.length()));
+                    wordInProgress = wordInProgress.append(
+                            potentialWord.charAt(gameWord.length()));
+                    //Jarvis has made a move.
+                    move = true;
+                    //Kill the loop.
                     break;
                 }
             }
         }
-        //Check if adding the letter creates a valid word and ends game.
-        if (checkWord(wordInProgress.toString()) == true) {
-            return "Game Over!  Victor: Player 1";
-        } else {
-            System.out.print(wordInProgress.toString() + "\n");
-            return wordInProgress.toString();
+
+        //Jarvis did not find a possible move or potential word.
+        //He must complete the word at hand and take the loss.
+        if (move == false) {
+            wordInProgress = wordInProgress.append(
+                    potentialWord.charAt(gameWord.length()));
         }
+
+        //Print Jarvis's move.
+        System.out.print("Player 2: " + wordInProgress.toString() + "\n");
+        return wordInProgress.toString();
+
     }
 }
